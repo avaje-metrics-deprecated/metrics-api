1be6ba33207861a8ae6e141637d919ada5cf209e
Rob Bygrave <robin.bygrave@gmail.com>
2016-12-05 17:09:47 +1300

    Add MetricName.of() helper methods #9

--- src/main/java/org/avaje/metric/MetricName.java
+++ src/main/java/org/avaje/metric/MetricName.java
@@ -6,6 +6,37 @@ package org.avaje.metric;
 public interface MetricName extends Comparable<MetricName> {
 
   /**
+   * Create a Metric name by parsing a name that is expected to include periods (dot notation
+   * similar to package.Class.method).
+   */
+  static MetricName of(String name) {
+    return MetricManager.name(name);
+  }
+
+  /**
+   * Create a MetricName based on a class and name.
+   * <p>
+   * Often the name maps to a method name.
+   */
+  static MetricName of(Class<?> cls, String name) {
+    return MetricManager.name(cls, name);
+  }
+
+  /**
+   * Create a Metric name based on group, type and name.
+   *
+   * @param group
+   *          The group which often maps to a package name.
+   * @param type
+   *          The type which often maps to a simple class name.
+   * @param name
+   *          The name which often maps to a method name.
+   */
+  static MetricName of(String group, String type, String name) {
+    return MetricManager.name(group, type, name);
+  }
+
+  /**
    * Returns the group to which the metric belongs. For class-based metrics, this will be the
    * package name.
    */
